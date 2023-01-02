0973d9bc75909e8486deb8bf0479b565dc9c841a
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-01-02 11:36:48 +0000

    data types

---
+++ basics01/02_data_types.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+// alert ( 3 + 3 ) // we are using nodejs , not browser
+
+// code readability should be hight
+console.log("hitesh")
+console.log(3+3)
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//Number => 2 to power 53 
+//bigint 
+//string => " "
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+// console.log(typeof "hitesh")
+console.log(typeof age) // number 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
