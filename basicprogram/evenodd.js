8cd05f56e113406b45548b2df96aa5cd1e846d54
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-02-03 22:09:13 +0530

    basic program1

---
+++ basicprogram/evenodd.js
@@ -0,0 +1,10 @@
+let num = 5;
+
+//Conditions to check if the number is negative or positive
+if (num > 0) {
+  console.log("The number is positive");
+} else if (num < 0) {
+  console.log("The number is negative");
+} else {
+  console.log("Zero");
+}
