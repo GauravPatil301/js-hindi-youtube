3104f2853c079caebecc8228e7233858f59c0c2e
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-02-06 15:48:02 +0530

    sumofNumber

--- basicprogram/sumofNumber.js
+++ basicprogram/sumofNumber.js
@@ -23,3 +23,36 @@ function getSum(n) {
 }
 
 console.log(getSum(n));
+
+
+/// Find the Sum of the Numbers in a Given Interval in Javascript
+
+//Using Brute Force
+
+let a = 5;
+let b = 10;
+let sum = 0;
+for(let i=a;i<=b;i++){
+  sum = sum + i;
+}
+console.log("The sum is "+ sum);
+
+
+//Using formula
+let num1 = 2;
+let num2 = 5;
+let sum = num2 * (num2 + 1) / 2 - num1 * (num1 + 1) / 2 + num1;
+console.log("The Sum is " + sum);
+
+// Using recursion
+let a = 5;
+let b = 10;
+
+function getSum(sum, i, b) {
+  if (i > b) {
+    return sum;
+  }
+  return i + getSum(sum, i + 1, b);
+}
+
+console.log("The sum is " + getSum(0, a, b));
