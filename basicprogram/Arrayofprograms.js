7532efe1f3882b8989f4ebc21f7435750a8d9587
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-03-02 21:20:01 +0530

    leader in array

---
+++ basicprogram/Arrayofprograms.js
@@ -0,0 +1,25 @@
+// Leaders in an array
+
+// Javascript Function to print leaders in an array
+  
+function printLeaders( arr, size)
+{
+    for (let i = 0; i < size; i++)
+    {
+        let j;
+        for (j = i+1; j < size; j++)
+        {
+            if (arr[i] <=arr[j])
+                break;
+        }    
+        if (j == size) // the loop didn't break
+            document.write(arr[i] + " ");
+  }
+}
+// driver code 
+  
+        let arr = [ 16, 17, 4, 3, 5, 2 ]; 
+        let n = arr.length; 
+  
+        // Function calling 
+        printLeaders(arr, n); 
