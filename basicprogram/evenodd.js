21a0a00292639417e38883a175529eddd08e7652
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-06-19 13:49:52 +0530

    new method add in evenodd.js

--- basicprogram/evenodd.js
+++ basicprogram/evenodd.js
@@ -8,18 +8,18 @@ if(number %2 == 0){
 
 
 //Method 2 ternary operator
-let number2 = 21;
-let result = number2% 2 == 0 ? ' is Even': ' is odd';
-console.log(number2+result) 
+// let number2 = 21;
+// let result = number2% 2 == 0 ? ' is Even': ' is odd';
+// console.log(number2+result) 
 
 //Method 3 bitwise operator
-// function isEven(number){
-//     return !(number & 1)
-// }
+function isEven(number){
+    return !(number & 1)
+}
 
-// let number = 29;
-// if(isEven(number)){
-//     console.log("Even")
-// }else{
-//     console.log("odd")
-// }
+let number = 29;
+if(isEven(number)){
+    console.log("Even")
+}else{
+    console.log("odd")
+}
