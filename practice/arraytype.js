a388a3c1480960679c987ce3ecf5c98d277ee307
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-06-21 18:17:14 +0530

    practice 2d array

---
+++ practice/arraytype.js
@@ -0,0 +1,26 @@
+// Insertion in array
+
+const arr = new Array(3);
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+console.log(arr.toString)
+
+console.log(arr[2])
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i])
+}
+
+let matrix = [[4,5],[34,45],[8,9]]
+for(let i=0;i<3;i++){
+    for(let j=0;j<2;j++){
+        console.log(`Matrix[${i}][${j}] = ${matrix[i][j]}`)
+    }
+}
+
+let matrix = [[4,5],[34,45],[8,9]]
+for(let i=0;i<3;i++){
+    for(let j=0;j<2;j++){
+        console.log(`Matrix[${i}][${j}] = ${matrix[i][j]}`)
+    }
+}
