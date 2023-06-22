b6e20826e1048b245fd097d5652c529b36bb26cc
GauravPatil301 <102862547+GauravPatil301@users.noreply.github.com>
2023-06-22 11:36:14 +0530

    practice file updated

--- practice/arraytype.js
+++ practice/arraytype.js
@@ -24,3 +24,22 @@ for(let i=0;i<3;i++){
         console.log(`Matrix[${i}][${j}] = ${matrix[i][j]}`)
     }
 }
+
+// Initializing while declaring
+let arr1 = new Array(3)
+arr1[0] = 10
+arr1[1] = 20
+arr1[2] = 30
+console.log("Array 1: ", arr1)
+
+// Creates an array having elements 10, 20, 30, 40, 50
+let arr2 = new Array(10, 20, 30, 40, 50);
+console.log("Array 2: ", arr2)
+
+// Creates an array of 5 undefined elements
+let arr3 = new Array(5);
+console.log("Array 3: ", arr3)
+
+// Creates an array with one element
+let arr4 = new Array("1BHK");
+console.log("Array 4: ", arr4)
